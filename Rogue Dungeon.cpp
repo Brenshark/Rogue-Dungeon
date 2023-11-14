dc6dbd9b1ad72afdd3be22f35a37d92c592b6e99
Brenshark <brensharkg@gmail.com>
2023-11-14 16:55:52 -0500

    Intro

--- Rogue Dungeon.cpp
+++ Rogue Dungeon.cpp
@@ -1,5 +1,26 @@
 ﻿#include "Rogue Dungeon.h"
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
+int main() {
+	int option;
+	cout << "1. Start Game" << endl;
+	cout << "2. Instructions" << endl;
+	cout << "3. Settings" << endl;
+	cout << "4. Quit" << endl;
+
+	cin >> option;
+	if (option == 1) {
+		cout << "does not function";
+	}
+	else if (option == 2) {
+		cout << "does not function";
+	}
+	else if (option == 3) {
+		cout << "does not function";
+	}
+	else if (option == 4) {
+		cout << "does not function";
+	}
+}
